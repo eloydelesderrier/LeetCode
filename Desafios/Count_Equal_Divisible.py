b5be186d3fbe5051e4fc38bdf06807dc2c79c625
eloydelesderrier <eloy.delesderrier@hotmail.com>
2025-04-23 14:29:18 -0300

    Contagem de pares iguais e divisíveis em uma matriz

--- Desafios/Count_Equal_Divisible.py
+++ Desafios/Count_Equal_Divisible.py
@@ -28,7 +28,7 @@ class Solution(object):
         n = len(nums)
         for i in range(n):
             for j in range(i+1, n):
-                if nums[i] == nums[j] and (i * j) % k ==0:
+                if nums[i] == nums[j] and (i * j) % k == 0:
                     count+=1
         
         return count
